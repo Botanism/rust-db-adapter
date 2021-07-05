cf34922eb4af28b0c5a8793944aecad277a687d7
s0lst1ce <thithib.cohergne@gmail.com>
2021-07-05 16:38:58 +0200

    improved test framework

--- tests/framework.rs
+++ tests/framework.rs
@@ -7,7 +7,7 @@ pub mod db_test_interface {
 
     use dotenv::dotenv;
     use rand::{thread_rng, Rng};
-    use sqlx::{Connection, PgConnection, Result};
+    use sqlx::{migrate, query, Connection, PgConnection, Result};
     use tokio::runtime::Runtime;
 
     pub fn db_session<F>(test: F) -> Result<()>
@@ -29,7 +29,7 @@ pub mod db_test_interface {
             }
         });
 
-        this_runtime.block_on(async { db_teardown(&db_url, &db_name).await })?;
+        this_runtime.block_on(async { teardown_db(&db_url, &db_name).await })?;
 
         assert!(result.is_ok());
 
@@ -41,25 +41,43 @@ pub mod db_test_interface {
         let mut name = String::from("BotanistTest_");
         let random_id: u128 = thread_rng().gen();
         name.push_str(random_id.to_string().as_str());
+        println!("{:?}", &name);
 
         let mut conn = PgConnection::connect(&db_url).await?;
-        sqlx::query(&format!("CREATE DATABASE {};", name))
-            //executor is only impl for &mut Connection
+        // TODO: investigave why using the `query!` macro would not compile
+        sqlx::query(&format!("CREATE TABLE {}", name))
+            //Executor is only impl for &mut Connection
             .execute(&mut conn)
             .await?;
+        apply_migrations(&mut conn).await?;
+        insert_dummy(&mut conn).await?;
 
         Ok(name)
     }
 
-    ///we drop tehe db after testing
-    async fn db_teardown(db_url: &str, name: &str) -> Result<()> {
+    ///we drop the db after testing
+    async fn teardown_db(db_url: &str, name: &str) -> Result<()> {
         let mut conn = PgConnection::connect(&db_url).await?;
 
-        sqlx::query(&format!("DROP DATABSE {};", name))
+        sqlx::query(&format!("DROP DATABASE {};", name))
             .execute(&mut conn)
             .await?;
         conn.close().await?;
 
+        Ok(())
+    }
+
+    /// we apply all the migrations from `migration` to our test DB
+    async fn apply_migrations(conn: &mut PgConnection) -> Result<()> {
+        migrate!("./migrations").run(conn).await?;
+        Ok(())
+    }
+
+    /// inserts some dummy values into the dabase to allow tests to be relevant
+    async fn insert_dummy(conn: &mut PgConnection) -> Result<()> {
+        query!("INSERT INTO guilds(id, welcome_message, goodbye_message, advertise, admin_chan, poll_chans, priv_manager, priv_admin, priv_event) VALUES (0, 'hello', NULL, true, 76543, array[5345345, 5764574], array[6675421, 2321390], array[6675421], array[46456]),(1, NULL, 'goodbye', false, 765430, array[53453450, 57645740], array[66754210, 23213900], array[66754210], array[464560])")
+            .execute(conn)
+            .await?;
         Ok(())
     }
 }
