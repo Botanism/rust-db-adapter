860f4c77740c7bd68b81d818a72300ae7d3e3c46
s0lst1ce <thithib.cohergne@gmail.com>
2021-07-05 16:38:58 +0200

    improved test framework

---
+++ migrations/20210702084948_create_guilds.sql
@@ -0,0 +1,11 @@
+-- guild support
+create table guilds(
+    id bigint primary key,
+    welcome_message varchar(2048),
+    goodbye_message varchar(2048),
+    advertise bool,
+    admin_chan bigint not null,
+    priv_manager bigint[], not null,
+    priv_admin bigint[], not null,
+    priv_event bigint[], not null
+)
