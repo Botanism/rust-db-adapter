2862cc3d42e36bf6c9f79179a750084f765719e5
s0lst1ce <thithib.cohergne@gmail.com>
2021-07-13 23:24:41 +0200

    add guild wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-use dotenv;
 use sqlx::postgres::PgPool;
 use std::env;
+use std::fmt::Write;
+use thiserror::Error;
 
 pub mod guild;
 
@@ -9,4 +10,18 @@ pub async fn establish_connection() -> PgPool {
     PgPool::connect(&env::var("DATABASE_URL").expect("DATABASE_URL was not set"))
         .await
         .expect("Could not establish connection")
+}
+
+pub(crate) fn as_pg_array(vec: Vec<i64>) -> String {
+    let mut array = String::new();
+    if vec.is_empty() {
+        return String::from("'{}'");
+    }
+    write!(array, "'{{").unwrap();
+    for int in vec {
+        write!(array, "{},", int).unwrap();
+    }
+    array.pop(); //removing the trailing comma
+    write!(array, "}}'").unwrap();
+    array
 }
--- tests/test_guild.rs
+++ tests/test_guild.rs
@@ -1,23 +1,219 @@
+use std::convert::TryFrom;
 mod framework;
-use db_adapter::guild::GuildConfig;
+use db_adapter::guild::{GuildConfig, Privilege};
 use framework::{db_test_interface::db_session, guild_test_info::*};
-use sqlx::{query, Connection, PgConnection, Result};
+use macro_rules_attribute::apply;
+use serenity::model::id::{ChannelId, RoleId};
+use sqlx::{PgPool, Result};
 
-#[test]
-fn test_get_welcome_message() -> Result<()> {
-    db_session(|db_url, runtime| {
-        runtime.block_on(async {
-            let mut conn = PgConnection::connect(&db_url).await?;
-            assert_eq!(
-                GuildConfig::from(FIRST_ID)
-                    .get_welcome_message(&mut conn)
-                    .await
-                    .unwrap()
-                    .unwrap()
-                    .as_str(),
-                FIRST_WELCOME_MESSAGE.unwrap()
-            );
-            Ok(())
-        })
-    })
+#[apply(db_test!)]
+async fn test_some_get_welcome_message(pool: PgPool) -> Result<()> {
+    assert_eq!(
+        GuildConfig::from(FIRST_ID)
+            .get_welcome_message(&pool)
+            .await
+            .unwrap()
+            .unwrap()
+            .as_str(),
+        FIRST_WELCOME_MESSAGE.unwrap()
+    );
+    Ok(())
+}
+
+#[apply(db_test!)]
+async fn test_none_get_welcome_message(pool: PgPool) -> Result<()> {
+    assert_eq!(
+        GuildConfig::from(SECOND_ID)
+            .get_welcome_message(&pool)
+            .await
+            .unwrap(),
+        SECOND_WELCOME_MESSAGE
+    );
+    Ok(())
+}
+
+#[apply(db_test!)]
+async fn test_set_welcome_message(pool: PgPool) -> Result<()> {
+    let g_config = GuildConfig::from(FIRST_ID);
+    g_config
+        .set_welcome_message(&pool, Some("welcome message"))
+        .await
+        .unwrap();
+    assert_eq!(
+        g_config.get_welcome_message(&pool).await.unwrap(),
+        Some("welcome message".to_string())
+    );
+    Ok(())
+}
+
+#[apply(db_test!)]
+async fn test_get_advertise(pool: PgPool) -> Result<()> {
+    assert_eq!(
+        GuildConfig::from(FIRST_ID)
+            .get_advertise(&pool)
+            .await
+            .unwrap(),
+        FIRST_ADVERTISE
+    );
+    Ok(())
+}
+
+#[apply(db_test!)]
+async fn test_set_advertise(pool: PgPool) -> Result<()> {
+    let g_config = GuildConfig::from(FIRST_ID);
+    g_config.set_advertise(&pool, false).await.unwrap();
+    assert_eq!(g_config.get_advertise(&pool).await.unwrap(), false);
+    Ok(())
+}
+
+#[apply(db_test!)]
+async fn test_some_get_admin_chan(pool: PgPool) -> Result<()> {
+    assert_eq!(
+        GuildConfig::from(FIRST_ID)
+            .get_admin_chan(&pool)
+            .await
+            .unwrap()
+            .unwrap(),
+        ChannelId(u64::try_from(FIRST_ADMIN_CHAN.unwrap()).unwrap())
+    );
+    Ok(())
+}
+
+#[apply(db_test!)]
+async fn test_none_get_admin_chan(pool: PgPool) -> Result<()> {
+    assert_eq!(
+        GuildConfig::from(SECOND_ID)
+            .get_admin_chan(&pool)
+            .await
+            .unwrap(),
+        SECOND_ADMIN_CHAN
+    );
+    Ok(())
+}
+
+#[apply(db_test!)]
+async fn test_set_admin_chan(pool: PgPool) -> Result<()> {
+    let g_config = GuildConfig::from(FIRST_ID);
+    g_config
+        .set_admin_chan(&pool, Some(ChannelId(1234567890)))
+        .await
+        .unwrap();
+    assert_eq!(
+        g_config.get_admin_chan(&pool).await.unwrap(),
+        Some(ChannelId(1234567890))
+    );
+    Ok(())
+}
+
+#[apply(db_test!)]
+async fn test_get_roles_with(pool: PgPool) -> Result<()> {
+    let g_config = GuildConfig::from(FIRST_ID);
+    assert_eq!(
+        g_config
+            .get_roles_with(&pool, Privilege::Admin)
+            .await
+            .unwrap(),
+        FIRST_PRIV_ADMIN
+    );
+    Ok(())
+}
+
+#[apply(db_test!)]
+async fn test_grant_admin_privilege(pool: PgPool) -> Result<()> {
+    let guild_conf = GuildConfig::from(FIRST_ID);
+    let role = RoleId(1234567);
+    guild_conf
+        .grant_privilege(&pool, role, Privilege::Admin)
+        .await
+        .unwrap();
+    //admin priv was given
+    //manager priv was also given (invariant check)
+    assert!(guild_conf
+        .has_privileges(&pool, role, &[Privilege::Admin, Privilege::Manager])
+        .await
+        .unwrap());
+
+    Ok(())
+}
+
+#[apply(db_test!)]
+async fn test_grant_any_privilege(pool: PgPool) -> Result<()> {
+    let guild_conf = GuildConfig::from(FIRST_ID);
+    let role = RoleId(1234567);
+    //any other priv than Admin
+    guild_conf
+        .grant_privilege(&pool, role, Privilege::Event)
+        .await
+        .unwrap();
+
+    assert!(guild_conf
+        .has_privilege(&pool, role, Privilege::Event)
+        .await
+        .unwrap());
+    Ok(())
+}
+
+#[apply(db_test!)]
+async fn test_deny_admin_privilege(pool: PgPool) -> Result<()> {
+    let guild_conf = GuildConfig::from(FIRST_ID);
+    guild_conf
+        .deny_privilege(&pool, FIRST_PRIV_ADMIN[0], Privilege::Admin)
+        .await
+        .unwrap();
+    assert!(!guild_conf
+        .has_privilege(&pool, FIRST_PRIV_ADMIN[0], Privilege::Admin)
+        .await
+        .unwrap());
+    Ok(())
+}
+
+#[apply(db_test!)]
+async fn test_have_privilege(pool: PgPool) -> Result<()> {
+    let guild_config = GuildConfig::from(FIRST_ID);
+    assert!(guild_config
+        .have_privilege(
+            &pool,
+            &[FIRST_PRIV_MANAGER[0], FIRST_PRIV_MANAGER[1]],
+            Privilege::Manager
+        )
+        .await
+        .unwrap());
+    Ok(())
+}
+
+#[apply(db_test!)]
+async fn test_has_privilege(pool: PgPool) -> Result<()> {
+    let guild_config = GuildConfig::from(FIRST_ID);
+    assert!(guild_config
+        .has_privilege(&pool, FIRST_PRIV_MANAGER[0], Privilege::Manager)
+        .await
+        .unwrap());
+    Ok(())
+}
+
+#[apply(db_test!)]
+async fn test_has_privileges(pool: PgPool) -> Result<()> {
+    let guild_config = GuildConfig::from(FIRST_ID);
+    assert!(guild_config
+        .has_privileges(
+            &pool,
+            FIRST_PRIV_ADMIN[0],
+            &[Privilege::Admin, Privilege::Manager]
+        )
+        .await
+        .unwrap());
+    Ok(())
+}
+
+#[apply(db_test!)]
+async fn test_get_privileges_for(pool: PgPool) -> Result<()> {
+    let guild_config = GuildConfig::from(FIRST_ID);
+    assert_eq!(
+        guild_config
+            .get_privileges_for(&pool, FIRST_PRIV_ADMIN[0])
+            .await
+            .unwrap(),
+        vec![Privilege::Admin, Privilege::Manager]
+    );
+    Ok(())
 }
