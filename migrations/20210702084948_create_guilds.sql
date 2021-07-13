a58312ea2a1dd585026c7c6bccdba0b4adece687
s0lst1ce <thithib.cohergne@gmail.com>
2021-07-13 23:59:13 +0200

    fix migration

--- migrations/20210702084948_create_guilds.sql
+++ migrations/20210702084948_create_guilds.sql
@@ -6,7 +6,7 @@ create table guilds(
     advertise bool,
     admin_chan bigint,
     poll_chans bigint[],
-    priv_manager bigint[] not null,
     priv_admin bigint[] not null,
+    priv_manager bigint[] not null,
     priv_event bigint[] not null
 )
